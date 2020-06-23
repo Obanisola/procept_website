5f8f9235d8bfcfcd5a8c1c02e15e6b32d7ec0367
Tobi Obanisola <tobiobanisola@gmail.com>
2020-06-23 12:51:48 +0100

    overlay - menu - overflow - script

--- js/overlay.js
+++ js/overlay.js
@@ -1,7 +1,9 @@
 function openNav() {
     document.getElementById("myNav").style.left = "0";
-}
+    document.body.style.overflow = 'hidden';
+ }
 
 function closeNav() {
     document.getElementById("myNav").style.left = "100%";
+    document.body.style.overflow = 'visible';
 }
